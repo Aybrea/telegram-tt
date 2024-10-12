a7d6a38c86c178d745f5abfc3fa43cd51986ab60
Aybrea <wrenchou22@gmail.com>
2024-10-12 10:23:26 +0800

    联系人列表加分类

--- src/components/left/main/ContactList.tsx
+++ src/components/left/main/ContactList.tsx
@@ -13,10 +13,17 @@ import useInfiniteScroll from '../../../hooks/useInfiniteScroll';
 import useOldLang from '../../../hooks/useOldLang';
 
 import PrivateChatInfo from '../../common/PrivateChatInfo';
+import Button from '../../ui/Button';
 import FloatingActionButton from '../../ui/FloatingActionButton';
 import InfiniteScroll from '../../ui/InfiniteScroll';
 import ListItem from '../../ui/ListItem';
 import Loading from '../../ui/Loading';
+
+import styles from './ContactList.module.scss';
+
+import aiRobotPath from '../../../assets/nIcons/ai-robot.svg';
+import friendApplyPath from '../../../assets/nIcons/friend-apply.svg';
+import groupChatPath from '../../../assets/nIcons/group-chat.svg';
 
 export type OwnProps = {
   filter: string;
@@ -67,42 +74,94 @@ const ContactList: FC<OwnProps & StateProps> = ({
 
   const [viewportIds, getMore] = useInfiniteScroll(undefined, listIds, Boolean(filter));
 
+  function sortList() {}
+
   return (
-    <InfiniteScroll items={viewportIds} onLoadMore={getMore} className="chat-list custom-scroll">
-      {viewportIds?.length ? (
-        viewportIds.map((id) => (
-          <ListItem
-            key={id}
-            className="chat-item-clickable contact-list-item"
-            // eslint-disable-next-line react/jsx-no-bind
-            onClick={() => handleClick(id)}
+    <>
+      <div className={styles.root}>
+        <div>
+          <Button
+            ripple={!isMobile}
+            round
+            pill
+            onClick={sortList}
+            size="default"
+            color="translucent"
           >
-            <PrivateChatInfo
-              userId={id}
-              forceShowSelf
-              avatarSize="large"
-              withStory
-              storyViewerOrigin={StoryViewerOrigin.ChatList}
-              ripple={!isMobile}
-            />
-          </ListItem>
-        ))
-      ) : viewportIds && !viewportIds.length ? (
-        <p className="no-results" key="no-results" dir="auto">
-          {filter.length ? 'No contacts matched your search.' : 'Contact list is empty.'}
-        </p>
-      ) : (
-        <Loading key="loading" />
-      )}
-      <FloatingActionButton
-        key="create-new-contact"
-        isShown
-        onClick={openNewContactDialog}
-        ariaLabel={lang('CreateNewContact')}
-      >
-        <i className="icon icon-add-user-filled" />
-      </FloatingActionButton>
-    </InfiniteScroll>
+            <img src={friendApplyPath} alt="" />
+          </Button>
+          <span>
+            {lang('NewFriend')}
+          </span>
+        </div>
+        <div>
+          <Button
+            ripple={!isMobile}
+            round
+            pill
+            onClick={sortList}
+            size="default"
+            color="translucent"
+          >
+            <img src={groupChatPath} alt="" />
+          </Button>
+          <span>
+            {lang('GroupChat')}
+          </span>
+        </div>
+        <div>
+          <Button
+            ripple={!isMobile}
+            round
+            pill
+            onClick={sortList}
+            size="default"
+            color="translucent"
+          >
+            <img src={aiRobotPath} alt="" />
+          </Button>
+          <span>
+            {lang('AI')}
+          </span>
+        </div>
+      </div>
+      <div className={styles.divider} />
+      <InfiniteScroll items={viewportIds} onLoadMore={getMore} className="chat-list custom-scroll">
+        {viewportIds?.length ? (
+          viewportIds.map((id) => (
+            <ListItem
+              key={id}
+              className="chat-item-clickable contact-list-item"
+              // eslint-disable-next-line react/jsx-no-bind
+              onClick={() => handleClick(id)}
+            >
+              <PrivateChatInfo
+                userId={id}
+                forceShowSelf
+                avatarSize="large"
+                withStory
+                storyViewerOrigin={StoryViewerOrigin.ChatList}
+                ripple={!isMobile}
+              />
+            </ListItem>
+          ))
+        ) : viewportIds && !viewportIds.length ? (
+          <p className="no-results" key="no-results" dir="auto">
+            {filter.length ? 'No contacts matched your search.' : 'Contact list is empty.'}
+          </p>
+        ) : (
+          <Loading key="loading" />
+        )}
+        <FloatingActionButton
+          key="create-new-contact"
+          isShown
+          onClick={openNewContactDialog}
+          ariaLabel={lang('CreateNewContact')}
+        >
+          <i className="icon icon-add-user-filled" />
+        </FloatingActionButton>
+      </InfiniteScroll>
+    </>
   );
 };
 
